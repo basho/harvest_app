2326a6860508b2683bab7ce44b52fe831d6741d6
Igor Justino de Souza <igor@zendesk.com>
2012-09-06 12:04:37 -0700

    Implement code review suggestions

--- Harvest/app.js
+++ Harvest/app.js
@@ -289,7 +289,9 @@
 
     // API returns text and status code 200 when request fails =/
     handleFailedRequest: function(jqXHR, textStatus, errorThrown) {
-      var message = textStatus === 'parsererror' ? this.I18n.t('invalidResponse') : this.I18n.t('problem', { error: jqXHR.responseText });
+      var message = textStatus === 'parsererror' ?
+                                    this.I18n.t('invalidResponse') :
+                                    this.I18n.t('problem', { error: jqXHR.responseText });
       this.showError(message);
     },
 
